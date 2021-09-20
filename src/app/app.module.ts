8a1e8ab397da86eb0d3581b57975a962396e0458
maiconm <maiconm23@gmail.com>
2021-09-20 08:56:09 -0300

    Cria `CartaComponent` em branco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaItensComponent } from './components/lista-itens/lista-itens.component';
+import { CartaComponent } from './components/carta/carta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaItensComponent,
+    CartaComponent,
   ],
   imports: [
     BrowserModule,
