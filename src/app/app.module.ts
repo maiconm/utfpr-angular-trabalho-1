d07c835c07712074ed103279e5ec45048eac21bc
maiconm <maiconm23@gmail.com>
2021-09-20 00:53:57 +0000

    Cria componente `ListaItens` em branco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaItensComponent } from './components/lista-itens/lista-itens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListaItensComponent,
   ],
   imports: [
     BrowserModule,
