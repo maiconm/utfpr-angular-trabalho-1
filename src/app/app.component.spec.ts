858f6e4972f30f3574395e51b01d03956a6c2f52
maiconm <maiconm23@gmail.com>
2021-09-26 21:55:10 +0000

    Implementa componentes `Carta` e `ListaItens`

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { Naipes } from './enums/naipes';
+import { Valores } from './enums/valores';
+import { Carta } from './models/carta';
+
+@Component({
+  selector: 'app-lista-itens',
+  template: ``,
+  styles: [],
+})
+class ListaItensComponentMock{
+  @Input()
+  public itens = [];
+  @Output()
+  public itemSelecionado = new EventEmitter<string>();
+}
+
+@Component({
+  selector: 'app-carta',
+  template: ``,
+  styles: [],
+})
+class CartaComponentMock{
+  @Input()
+  public carta = new Carta();
+}
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,14 +41,32 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+        ListaItensComponentMock,
+        CartaComponentMock,
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('ao iniciar, `carta` não pode ter valor nem naipe', () => {
+    expect(component.carta.valor).toBeFalsy();
+    expect(component.carta.naipe).toBeFalsy();
+  });
+
+  it('`carta` deve receber o naipe e valor escolhido', () => {
+    component.selecionarNaipe(component.naipes[3]);
+    component.selecionarValor(component.valores[3]);
+    expect(component.carta.naipe).toBe(Naipes.espadas);
+    expect(component.carta.valor).toBe(Valores.quatro);
   });
 
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Naipes } from './enums/naipes';
+import { Valores } from './enums/valores';
+import { Carta } from './models/carta';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  /**
+   * Valores das cartas.
+   */
+  public valores = Object.values(Valores);
+  /**
+   * Naipes das cartas.
+   */
+  public naipes = Object.values(Naipes);
+  /**
+   * Objeto carta com os valores e napes selecionados.
+   */
+  public carta = new Carta();
+  /**
+   * Seleciona valor da carta.
+   * @param valor Valor.
+   */
+  public selecionarValor(valor: string): void {
+    this.carta.valor = <Valores>valor;
+  }
+  /**
+   * Seleciona o naipe da carta.
+   * @param naipe Naipe.
+   */
+  public selecionarNaipe(naipe: string): void {
+    this.carta.naipe = <Naipes>naipe;
+  }
+
 }
