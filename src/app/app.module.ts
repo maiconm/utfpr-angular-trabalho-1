8c2fc4557af13e54a05cfc979dfe1786e6a4cd97
maiconm <maiconm23@gmail.com>
2021-09-26 20:22:34 +0000

    Cria `CorCartaPipe` em branco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaItensComponent } from './components/lista-itens/lista-itens.component';
 import { CartaComponent } from './components/carta/carta.component';
+import { CorCartaPipe } from './pipes/cor-carta.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaItensComponent,
     CartaComponent,
+    CorCartaPipe,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/cor-carta.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CorCartaPipe } from './cor-carta.pipe';
+
+describe('CorCartaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CorCartaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
